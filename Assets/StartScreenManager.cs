5e3d4c1c40208f9f45a35c241ebbdbf3dc37a847
JiyaVe <jiyaverma22@iitk.ac.in>
2023-10-28 10:01:02 +0530

    StartScreen

---
+++ Assets/StartScreenManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StartScreenManager : MonoBehaviour
+{
+
+
+   public void MainMenu()
+   {
+    SceneManager.LoadScene(1);
+   }
+       
+
+        
+}
+
+
