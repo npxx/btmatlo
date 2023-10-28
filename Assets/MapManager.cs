bb0336b2732635e27882c3d5f43f1e034efe5a60
JiyaVe <jiyaverma22@iitk.ac.in>
2023-10-28 23:16:43 +0530

    Map

---
+++ Assets/MapManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+   public void library()
+   {
+        SceneManager.LoadScene(3);
+   }
+
+    public void ncl()
+   {
+        SceneManager.LoadScene(4);
+   }
+
+
+}
