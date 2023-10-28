60db19e829ec3b756c899816b2e1f0207fbb9b59
npxx <96121824+npxx@users.noreply.github.com>
2023-10-28 23:25:34 +0530

    Added Task1 and NCLScene

---
+++ Assets/Scripts/Quanttask.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quanttask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    List<GameObject> bitss;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(bitss[0].GetComponent<TMP_Text>().text);
+        string t1 = bitss[0].GetComponent<TMP_Text>().text;
+        string t2 = bitss[1].GetComponent<TMP_Text>().text;
+        string t3 = bitss[2].GetComponent<TMP_Text>().text;
+        string t4 = bitss[3].GetComponent<TMP_Text>().text;
+        string t5 = bitss[4].GetComponent<TMP_Text>().text;
+        string t6 = bitss[5].GetComponent<TMP_Text>().text;
+        string t7 = bitss[6].GetComponent<TMP_Text>().text;
+        string t8 = bitss[7].GetComponent<TMP_Text>().text;
+      
+
+        if ((t3 == t7) && (t7 == t8) && (t8 == "0") && (t1 == t2) && (t2 == t4) && (t4 == t5) && (t5 == t6) && (t6 == "1"))
+        {
+            Debug.Log("okayy");
+        }
+        else
+        {
+            Debug.Log("bruh");
+        }
+    }
+
+    public void click()
+    {
+        Debug.Log("clickho");
+        TMP_Text a = transform.GetComponentInChildren<TMP_Text>();
+        Debug.Log(a);
+        if(a.text == "1") a.text = "0";
+        else a.text = "1";
+    }
+}
--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -10,6 +10,9 @@ public class Task : MonoBehaviour
 
     [SerializeField]
     SpriteRenderer taskren;
+
+    [SerializeField]
+    GameObject taskk;
     void Start()
     {
         
@@ -25,11 +28,16 @@ public class Task : MonoBehaviour
     {
         taskren.sprite = high;
         Debug.Log("collidee");
+        Invoke("tasktog", 1f);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         taskren.sprite = low;
         Debug.Log("exx");
+        Invoke("tasktog", 1f);
     }
+
+    public void tasktog()
+    { taskk.SetActive(!taskk.activeSelf); }
 }
